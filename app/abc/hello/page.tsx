00ca87c349393a1be32feeec6a77fe1c8a8968c0
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-12-06 22:15:54 +0530

    Deleting the files can refer through commit messages

--- app/abc/hello/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const abcRoute = () => {
-  return (
-    <div>Hello from abc route</div>
-  )
-}
-
-export default abcRoute
--- app/blog/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BlogPage = () => {
-  return (
-    <div>hello from BlogPage</div>
-  )
-}
-
-export default BlogPage
