89df3ed1805d40f052f1227cd1484c879a9bb6f2
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-12-09 11:50:01 +0530

    Create feature frontend and Backend using Mutation (Convex)

---
+++ app/(shared-layout)/create/page.tsx
@@ -0,0 +1,112 @@
+"use client"
+import { postSchema } from "@/app/schemas/blog";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { api } from "@/convex/_generated/api";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "convex/react";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+import z from "zod"
+
+export default function CreateDefault() {   
+
+    const[isPending, startTransition] = useTransition();
+    const router = useRouter();
+
+    //calling the end point
+    const mutation = useMutation(api.post.createPost)
+
+    const form = useForm({
+        resolver: zodResolver(postSchema),
+        defaultValues : {
+            content: "",
+            title: ""
+        }
+    })
+
+    //here we are passing postSchema
+    function onSubmit(values: z.infer<typeof postSchema>) {
+
+        startTransition(() => {
+            console.log(postSchema)
+
+        //calling our backend (mutation) from client
+            mutation({
+                body: values.content,
+                title: values.title,
+            })
+
+            toast.success('Everything was fine')
+
+            router.push('/')
+        })
+        
+    }
+
+    return(
+        <div className="py-12">
+            <div className="text-center mb-12">
+                <h1 className="text-4xl font-extrabold  tracking-tight sm:text-5xl">Create post</h1>
+                <p className="text-xl text-muted-foreground pt-4">Share your thoughts with the big world</p>
+
+            </div>
+
+            <Card className="w-full max-w-xl mx-auto">
+                <CardHeader>
+                    <CardTitle>Create a blog title</CardTitle>
+                    <CardDescription>Create a new blog article</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <FieldGroup className="gap-y-4">
+                            <Controller 
+                            name="title"
+                            control={form.control} 
+                            render={({field, fieldState}) => (
+                                <Field>
+                                    <FieldLabel>Title</FieldLabel>
+                                    <Input aria-invalid={fieldState.invalid} 
+                                            placeholder="super cool title"{...field}/>
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]}/>
+                                    )}
+                                </Field>
+                            )}/>
+                            <Controller 
+                            name="content"
+                            control={form.control} 
+                            render={({field, fieldState}) => (
+                                <Field>
+                                    <FieldLabel>Content</FieldLabel>
+                                    <Textarea aria-invalid={fieldState.invalid} 
+                                            placeholder="super cool blog content"{...field}/>
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]}/>
+                                    )}
+                                </Field>
+                            )}/>
+                            <Button disabled={isPending}>
+                            {isPending ? (
+                                <>
+                                    <Loader2 className="size-4 animate-spin"/>
+                                    <span>Loading</span>
+                                </>
+                            ) : (
+                                <span>Create Post</span>
+                            )}
+                        </Button>
+                        </FieldGroup>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
