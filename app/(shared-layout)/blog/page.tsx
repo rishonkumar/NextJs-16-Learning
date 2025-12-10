d5986e7907e6cb162c7f71fcafa73de20ea319bd
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-12-10 20:49:49 +0530

    Code optimizing and adding a Skeleton view unitll data is received

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -1,43 +1,76 @@
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 
-export default async function BlogPage() {
-
-    const data = await fetchQuery(api.post.getPosts)
+export default function BlogPage() {
 
 
     return (
         <div className="py-12">
             {/* {JSON.stringify(data)} */}
-            {/* <p>{data?.[0].title}</p> */}
             <div className="text-center pb-12">
                 <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl">Our blog</h1>
                 <p className="pt-4 max-w-2xl mx-auto text-xl text-muted-foreground">Insights, thoughts and trends from our team</p>
             </div>
 
-            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-                {data?.map((post) => (
-                    <Card key={post._id} className="pt-0 ">
-                        <div className="relative h-48 w-full overflow-hidden">
-                            <Image className="rounded-t-lg" src="https://images.unsplash.com/photo-1759979702262-71e15fa210d4?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt={post.title} fill />
-                        </div>
+            {/* While this renders it will show Loading till the data is received from backend this 
+            does not affect other components ex Navbar get loaded instatanlu */}
+            <Suspense fallback={<SkeletonLoadingUi />}>
+                <LoadBlogList />
+            </Suspense>
 
-                        <CardContent>
-                            <Link href={`/blog/${post._id}`}>
-                                <h1 className="text-2xl font-bold hover:text-primary">{post.title}</h1>
-                            </Link>
-                            <p className="text-muted-foreground line-clamp-3">{post.body}</p>
-                        </CardContent>
-                        <CardFooter>
-                            <Link className={buttonVariants({ className: "w-full" })} href={`/blog/${post._id}`}>Read more</Link>
-                        </CardFooter>
-                    </Card>
-                ))}
-            </div>
         </div>
     );
+}
+
+async function LoadBlogList() {
+    //added this for example to show how sekelton can be viewed in UI
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const data = await fetchQuery(api.post.getPosts)
+
+    return (
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {data?.map((post) => (
+                <Card key={post._id} className="pt-0 ">
+                    <div className="relative h-48 w-full overflow-hidden">
+                        <Image className="rounded-t-lg" src="https://images.unsplash.com/photo-1759979702262-71e15fa210d4?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt={post.title} fill />
+                    </div>
+
+                    <CardContent>
+                        <Link href={`/blog/${post._id}`}>
+                            <h1 className="text-2xl font-bold hover:text-primary">{post.title}</h1>
+                        </Link>
+                        <p className="text-muted-foreground line-clamp-3">{post.body}</p>
+                    </CardContent>
+                    <CardFooter>
+                        <Link className={buttonVariants({ className: "w-full" })} href={`/blog/${post._id}`}>Read more</Link>
+                    </CardFooter>
+                </Card>
+            ))}
+        </div>
+    )
+}
+
+function SkeletonLoadingUi() {
+    return (
+        <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-3">
+            {
+                [...Array(6)].map((_, i) => (
+                    <div key={i} className="flex flex-col space-y-3">
+                        <Skeleton className="h-48 w-full rounded-xl" />
+                        <div className="space-y-2 flex flex-col">
+                            <Skeleton className="h-6 w-3/4" />
+                            <Skeleton className="h-4 w-full" />
+                            <Skeleton className="h-4 w-2/3" />
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
